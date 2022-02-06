b273ae75cdfa55d7caafce47163fee5b97c31d7b
Rishabh Arya <rishabh.arya.ece19@itbhu.ac.in>
2022-02-06 21:39:33 +0530

    Add files via upload

---
+++ Test-2.py
@@ -0,0 +1,338 @@
+#-*- coding: utf8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.style.use('seaborn-deep')
+plt.style.use('fivethirtyeight')
+plt.rcParams['font.size'] = 14
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['axes.titlesize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['figure.titlesize'] = 14
+plt.rcParams['figure.figsize'] = (16,14)
+#Initial permut matrix for the datas
+PI = [58, 50, 42, 34, 26, 18, 10, 2,
+      60, 52, 44, 36, 28, 20, 12, 4,
+      62, 54, 46, 38, 30, 22, 14, 6,
+      64, 56, 48, 40, 32, 24, 16, 8,
+      57, 49, 41, 33, 25, 17, 9, 1,
+      59, 51, 43, 35, 27, 19, 11, 3,
+      61, 53, 45, 37, 29, 21, 13, 5,
+      63, 55, 47, 39, 31, 23, 15, 7]
+
+#Initial permut made on the key
+CP_1 = [57, 49, 41, 33, 25, 17, 9,
+        1, 58, 50, 42, 34, 26, 18,
+        10, 2, 59, 51, 43, 35, 27,
+        19, 11, 3, 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15,
+        7, 62, 54, 46, 38, 30, 22,
+        14, 6, 61, 53, 45, 37, 29,
+        21, 13, 5, 28, 20, 12, 4]
+
+#Permut applied on shifted key to get Ki+1
+CP_2 = [14, 17, 11, 24, 1, 5, 3, 28,
+        15, 6, 21, 10, 23, 19, 12, 4,
+        26, 8, 16, 7, 27, 20, 13, 2,
+        41, 52, 31, 37, 47, 55, 30, 40,
+        51, 45, 33, 48, 44, 49, 39, 56,
+        34, 53, 46, 42, 50, 36, 29, 32]
+
+#Expand matrix to get a 48bits matrix of datas to apply the xor with Ki
+E = [32, 1, 2, 3, 4, 5,
+     4, 5, 6, 7, 8, 9,
+     8, 9, 10, 11, 12, 13,
+     12, 13, 14, 15, 16, 17,
+     16, 17, 18, 19, 20, 21,
+     20, 21, 22, 23, 24, 25,
+     24, 25, 26, 27, 28, 29,
+     28, 29, 30, 31, 32, 1]
+
+#SBOX
+S_BOX = [
+         
+[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+ [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+ [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+ [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+],
+
+[[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+ [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+ [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+ [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+],
+
+[[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+ [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+ [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+ [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+],
+
+[[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+ [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+ [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+ [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+],  
+
+[[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+ [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+ [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+ [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+], 
+
+[[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+ [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+ [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+ [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+], 
+
+[[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+ [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+ [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+ [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+],
+   
+[[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+ [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+ [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+ [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+]
+]
+
+#Permut made after each SBox substitution for each round
+P = [16, 7, 20, 21, 29, 12, 28, 17,
+     1, 15, 23, 26, 5, 18, 31, 10,
+     2, 8, 24, 14, 32, 27, 3, 9,
+     19, 13, 30, 6, 22, 11, 4, 25]
+
+#Final permut for datas after the 16 rounds
+PI_1 = [40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25]
+
+#Matrix that determine the shift for each round of keys
+SHIFT = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
+
+def string_to_bit_array(text):#Convert a string into a list of bits
+    array = list()
+    for char in text:
+        binval = binvalue(char, 8)#Get the char value on one byte
+        array.extend([int(x) for x in list(binval)]) #Add the bits to the final list
+    return array
+
+def bit_array_to_string(array): #Recreate the string from the bit array
+    res = ''.join([chr(int(y,2)) for y in [''.join([str(x) for x in _bytes]) for _bytes in  nsplit(array,8)]])   
+    return res
+
+def binvalue(val, bitsize): #Return the binary value as a string of the given size 
+    binval = bin(val)[2:] if isinstance(val, int) else bin(ord(val))[2:]
+    if len(binval) > bitsize:
+        raise "binary value larger than the expected size"
+    while len(binval) < bitsize:
+        binval = "0"+binval #Add as many 0 as needed to get the wanted size
+    return binval
+
+def nsplit(s, n):#Split a list into sublists of size "n"
+    return [s[k:k+n] for k in range(0, len(s), n)]
+
+ENCRYPT=1
+DECRYPT=0
+
+class des():
+    def __init__(self):
+        self.password = None
+        self.text = None
+        self.keys = list()
+        
+    def run(self, key, text, action=ENCRYPT, padding=False):
+        if len(key) < 8:
+            raise "Key Should be 8 bytes long"
+        elif len(key) > 8:
+            key = key[:8] #If key size is above 8bytes, cut to be 8bytes long
+        
+        self.password = key
+        self.text = text
+        
+        if padding and action==ENCRYPT:
+            self.addPadding()
+        elif len(self.text) % 8 != 0:#If not padding specified data size must be multiple of 8 bytes
+            raise "Data size should be multiple of 8"
+        
+        self.generatekeys() #Generate all the keys
+        text_blocks = nsplit(self.text, 8) #Split the text in blocks of 8 bytes so 64 bits
+        result = list()
+        for block in text_blocks:#Loop over all the blocks of data
+            block = string_to_bit_array(block)#Convert the block in bit array
+            block = self.permut(block,PI)#Apply the initial permutation
+            g, d = nsplit(block, 32) #g(LEFT), d(RIGHT)
+            tmp = None
+            for i in range(16): #Do the 16 rounds
+                d_e = self.expand(d, E) #Expand d to match Ki size (48bits)
+                if action == ENCRYPT:
+                    tmp = self.xor(self.keys[i], d_e)#If encrypt use Ki
+                else:
+                    tmp = self.xor(self.keys[15-i], d_e)#If decrypt start by the last key
+                tmp = self.substitute(tmp) #Method that will apply the SBOXes
+                tmp = self.permut(tmp, P)
+                tmp = self.xor(g, tmp)
+                g = d
+                d = tmp
+                bit_sequence_roundwise[i][:32] = g
+                bit_sequence_roundwise[i][32:] = d
+            result += self.permut(d+g, PI_1) #Do the last permut and append the result to result
+        final_res = bit_array_to_string(result)
+        if padding and action==DECRYPT:
+            return self.removePadding(final_res) #Remove the padding if decrypt and padding is true
+        else:
+            return final_res #Return the final string of data ciphered/deciphered
+    
+    def substitute(self, d_e):#Substitute bytes using SBOX
+        subblocks = nsplit(d_e, 6)#Split bit array into sublist of 6 bits
+        result = list()
+        for i in range(len(subblocks)): #For all the sublists
+            block = subblocks[i]
+            row = int(str(block[0])+str(block[5]),2)#Get the row with the first and last bit
+            column = int(''.join([str(x) for x in block[1:][:-1]]),2) #Column is the 2,3,4,5th bits
+            val = S_BOX[i][row][column] #Take the value in the SBOX appropriated for the round (i)
+            bin = binvalue(val, 4)#Convert the value to binary
+            result += [int(x) for x in bin]#And append it to the resulting list
+        return result
+        
+    def permut(self, block, table):#Permut the given block using the given table (so generic method)
+        return [block[x-1] for x in table]
+    
+    def expand(self, block, table):#Do the exact same thing than permut but for more clarity has been renamed
+        return [block[x-1] for x in table]
+    
+    def xor(self, t1, t2):#Apply a xor and return the resulting list
+        return [x^y for x,y in zip(t1,t2)]
+    
+    def generatekeys(self):#Algorithm that generates all the keys
+        self.keys = []
+        key = string_to_bit_array(self.password)
+        key = self.permut(key, CP_1) #Apply the initial permut on the key
+        g, d = nsplit(key, 28) #Split it in to (g->LEFT),(d->RIGHT)
+        for i in range(16):#Apply the 16 rounds
+            g, d = self.shift(g, d, SHIFT[i]) #Apply the shift associated with the round (not always 1)
+            tmp = g + d #Merge them
+            self.keys.append(self.permut(tmp, CP_2)) #Apply the permut to get the Ki
+
+    def shift(self, g, d, n): #Shift a list of the given value
+        return g[n:] + g[:n], d[n:] + d[:n]
+    
+    def addPadding(self):#Add padding to the datas using PKCS5 spec.
+        pad_len = 8 - (len(self.text) % 8)
+        self.text += pad_len * chr(pad_len)
+    
+    def removePadding(self, data):#Remove the padding of the plain text (it assume there is padding)
+        pad_len = ord(data[-1])
+        return data[:-pad_len]
+    
+    def encrypt(self, key, text, padding=False):
+        return self.run(key, text, ENCRYPT, padding)
+    
+    def decrypt(self, key, text, padding=False):
+        return self.run(key, text, DECRYPT, padding)
+    
+if __name__ == '__main__':
+    # The key used for encryption
+    key = "00011100"
+    # The plaintext used for encryption
+    text= "10100100"
+    # The below key_list contains keys with hamming distances 1, 2, 3, 4 and 5 with respect to original key.
+    key_list = ["p0011100","°0011100","°0451±00","d0451±00","µút11100"]
+    # Dummy variable which will be storing the bit sequences roundwise
+    bit_sequence_roundwise = np.zeros((16,64))
+    # Dummy variable for storing the bit sequences of 5 different plaintext roundwise
+    plaintext_bit_sequence_roundwise = np.zeros((5,16,64))
+    # Dummy variables for storing hamming distances for plaintext1, plaintext2 and so on.
+    hamming_changes_1 = np.zeros((16),dtype=int)
+    hamming_changes_2 = np.zeros((16),dtype=int)
+    hamming_changes_3 = np.zeros((16),dtype=int)
+    hamming_changes_4 = np.zeros((16),dtype=int)
+    hamming_changes_5 = np.zeros((16),dtype=int)
+    print("The original plaintext : ",text)
+    print("The bit sequence : ",string_to_bit_array(text))
+    print("The original key : ",key)
+    print("The bit sequence : ",string_to_bit_array(key))
+    
+    
+    d = des()
+    r = d.encrypt(key,text)
+    # Storing the original bit sequence roundwise
+    org_plaintext_bit_sequence_roundwise = bit_sequence_roundwise
+    # r2 = d.decrypt(key,r)
+    print("Ciphered: %r" % r)
+    # print("Deciphered: ", r2)
+    # Storing the hamming distance for 5 plaintexts
+    for i in range(len(key_list)):
+        K = key_list[i]
+        bit_sequence_roundwise = np.zeros((16,64))
+        r = d.encrypt(K,text)
+        plaintext_bit_sequence_roundwise[i][:][:] = bit_sequence_roundwise
+        print("The key with hamming distance {} : {}".format(i+1,K))
+        print("The bit sequence : ",string_to_bit_array(K))
+    
+    for i in range(16):
+        cnt1 = 0
+        cnt2 = 0
+        cnt3 = 0
+        cnt4 = 0
+        cnt5 = 0
+        for j in range(64):
+            if org_plaintext_bit_sequence_roundwise[i][j] != plaintext_bit_sequence_roundwise[0][i][j] :
+                cnt1 += 1
+            if org_plaintext_bit_sequence_roundwise[i][j] != plaintext_bit_sequence_roundwise[1][i][j] :
+                cnt2 += 1
+            if org_plaintext_bit_sequence_roundwise[i][j] != plaintext_bit_sequence_roundwise[2][i][j] :
+                cnt3 += 1
+            if org_plaintext_bit_sequence_roundwise[i][j] != plaintext_bit_sequence_roundwise[3][i][j] :
+                cnt4 += 1
+            if org_plaintext_bit_sequence_roundwise[i][j] != plaintext_bit_sequence_roundwise[4][i][j] :
+                cnt5 += 1
+        hamming_changes_1[i] = cnt1
+        hamming_changes_2[i] = cnt2
+        hamming_changes_3[i] = cnt3
+        hamming_changes_4[i] = cnt4
+        hamming_changes_5[i] = cnt5
+    # Dataframe for presenting hamming distance variations in form of a table
+    avalanche = pd.DataFrame(np.c_[hamming_changes_1,hamming_changes_2,hamming_changes_3,hamming_changes_4,hamming_changes_5],
+                  columns=["Δp1","Δp2","Δp3","Δp4","Δp5"])
+    rounds = np.arange(1,17,1)
+    # Printing the table
+    print(avalanche)
+    # Line plot (Hamming distance VS No. of rounds)
+    plt.figure(figsize=(16,14))
+    plt.plot(rounds,avalanche["Δp1"],color="k",label="Δp1")
+    plt.plot(rounds,avalanche["Δp2"],color="#db222a",label="Δp2")
+    plt.plot(rounds,avalanche["Δp3"],color="#ffd60a",label="Δp3")
+    plt.plot(rounds,avalanche["Δp4"],color="#00b4d8",label="Δp4")
+    plt.plot(rounds,avalanche["Δp5"],color="#2d6a4f",label="Δp5")
+    plt.xlabel("No. of Rounds")
+    plt.ylabel("Hamming distance")
+    plt.title("Avalanche Effect",fontsize=24)
+    plt.legend()
+
+    # Pair plot (Hamming distance VS No. of rounds)
+    plt.figure(figsize=(16,14))
+    med = np.median(avalanche,axis=1)
+    sns.lineplot(data=med,linewidth=4,color="#A0001C")
+    sns.pointplot(data=avalanche.T,capsize=0.2,estimator=np.median,palette="icefire")
+    plt.title("Avalanche Effect",fontsize=24)
+    plt.xlabel("No. of Rounds",fontsize=16)
+    plt.ylabel("Hamming distance",fontsize=16)
+    plt.show()
+    
+
+
+    
